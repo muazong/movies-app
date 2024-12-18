41b786839dbab5a61094e3e75b7ce4a6d472e991
muazong <giongmua2004@gmail.com>
2024-12-18 23:29:20 +0700

    finish header UI

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,28 @@
-import "./Header.css";
+import { NavLink } from "react-router-dom";
+import "./Header.scss";
 
 function Header() {
-  return <div>Header</div>;
+  return (
+    <div className="header">
+      <div className="logo">
+        <h1>AZNG</h1>
+      </div>
+      <div className="navigation">
+        <NavLink
+          className={({ isActive }) => (isActive ? "active" : "")}
+          to={"/"}
+        >
+          Home
+        </NavLink>
+        <NavLink to={"/popular"}>Popular</NavLink>
+        <NavLink to={"/tv_shows"}>TV Shows</NavLink>
+        <NavLink to={"/about"}>About</NavLink>
+      </div>
+      <div className="search">
+        <input type="text" placeholder="Search for movies" />
+      </div>
+    </div>
+  );
 }
 
 export default Header;
