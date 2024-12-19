76bea9b7d36532b9be41bf2ce8232b68ef6e99e7
muazong <giongmua2004@gmail.com>
2024-12-19 08:38:35 +0700

    add error page

---
+++ src/pages/NotFound/NotFound.jsx
@@ -0,0 +1,11 @@
+import "./NotFound.scss";
+
+function NotFound() {
+  return (
+    <div className="not-found">
+      <h1>Oops, page not found!</h1>
+    </div>
+  );
+}
+
+export default NotFound;
