eb7c0701b9a520a0a6165b45d7f24e286081420b
muazong <giongmua2004@gmail.com>
2024-12-19 08:38:57 +0700

    update error page

--- src/Router/Router.jsx
+++ src/Router/Router.jsx
@@ -4,12 +4,13 @@ import Home from "../pages/Home";
 import Popular from "../pages/Popular";
 import TvShows from "../pages/TvShows";
 import About from "../pages/About";
+import NotFound from "../pages/NotFound";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Home />,
-    errorElement: <h1>Not Found!</h1>,
+    errorElement: <NotFound />,
   },
   {
     path: "/popular",
