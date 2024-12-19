5441bf48f4d9049170faf6588a79dd0c1168d353
muazong <giongmua2004@gmail.com>
2024-12-19 08:28:05 +0700

    update router

--- src/Router/Router.jsx
+++ src/Router/Router.jsx
@@ -1,8 +1,33 @@
 import PropTypes from "prop-types";
-import { BrowserRouter } from "react-router-dom";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
-function Router({ children }) {
-  return <BrowserRouter>{children}</BrowserRouter>;
+import Home from "../pages/Home";
+import Popular from "../pages/Popular";
+import TvShows from "../pages/TvShows";
+import About from "../pages/About";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+    errorElement: <h1>Not Found!</h1>,
+  },
+  {
+    path: "/popular",
+    element: <Popular />,
+  },
+  {
+    path: "/tv_shows",
+    element: <TvShows />,
+  },
+  {
+    path: "/about",
+    element: <About />,
+  },
+]);
+
+function Router() {
+  return <RouterProvider router={router} />;
 }
 
 Router.propTypes = {
