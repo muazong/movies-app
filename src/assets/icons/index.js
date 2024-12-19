f80f56ececa4273134ee57ce1d59ea8759ce0e6d
muazong <giongmua2004@gmail.com>
2024-12-19 21:51:09 +0700

    add more icons

--- src/assets/icons/index.js
+++ src/assets/icons/index.js
@@ -1,9 +1,24 @@
 import search_icon from "./magnifying-glass-solid.svg";
 import play_icon from "./play-solid.svg";
+import location_icon from "./location-dot-solid.svg";
+import email_icon from "./envelope-solid.svg";
+import phone_icon from "./phone-solid.svg";
+
+import facebook_icon from "./facebook-brands-solid.svg";
+import youtube_icon from "./youtube-brands-solid.svg";
+import x_twitter_icon from "./x-twitter-brands-solid.svg";
+import snapchat_icon from "./square-snapchat-brands-solid.svg";
 
 const icons = {
   search_icon,
   play_icon,
+  location_icon,
+  email_icon,
+  phone_icon,
+  facebook_icon,
+  youtube_icon,
+  x_twitter_icon,
+  snapchat_icon,
 };
 
 export default icons;
