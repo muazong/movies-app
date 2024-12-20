5c348904f410e309005cf01304e00255868fdb40
muazong <giongmua2004@gmail.com>
2024-12-20 09:25:37 +0700

    update icons

--- src/assets/icons/index.js
+++ src/assets/icons/index.js
@@ -7,7 +7,7 @@ import phone_icon from "./phone-solid.svg";
 import facebook_icon from "./facebook-brands-solid.svg";
 import youtube_icon from "./youtube-brands-solid.svg";
 import x_twitter_icon from "./x-twitter-brands-solid.svg";
-import snapchat_icon from "./square-snapchat-brands-solid.svg";
+import snapchat_icon from "./snapchat-brands-solid.svg";
 
 const icons = {
   search_icon,
