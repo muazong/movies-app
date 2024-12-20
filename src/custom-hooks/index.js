c0e1cf7ee86d7e99cc96a578860cd40f8bd49879
muazong <giongmua2004@gmail.com>
2024-12-21 05:06:00 +0700

    add more custom hook for trailer page

--- src/custom-hooks/index.js
+++ src/custom-hooks/index.js
@@ -1,16 +1,17 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import { useEffect, useState } from "react";
 import { API_KEY, URL } from "../assets/tokens";
 
+const options = {
+  method: "GET",
+  headers: {
+    accept: "application/json",
+    Authorization: `Bearer ${API_KEY}`,
+  },
+};
+
 function useMoviesFetch(type) {
   const [movieData, setMovieData] = useState([]);
-
-  const options = {
-    method: "GET",
-    headers: {
-      accept: "application/json",
-      Authorization: `Bearer ${API_KEY}`,
-    },
-  };
 
   const handleFetch = async () => {
     try {
@@ -30,4 +31,25 @@ function useMoviesFetch(type) {
   return movieData;
 }
 
-export { useMoviesFetch };
+function useTrailerFetch(id) {
+  const [trailerData, setTrailerData] = useState({});
+
+  const handleFetch = async () => {
+    try {
+      const response = await fetch(
+        `${URL}movie/${id}/videos?language=en-US`,
+        options,
+      );
+      const data = await response.json();
+      setTrailerData(data.results[0]);
+    } catch (error) {
+      alert("Error fetching movies" + error);
+    }
+  };
+
+  useEffect(() => handleFetch, []);
+
+  return trailerData;
+}
+
+export { useMoviesFetch, useTrailerFetch };
