5d3f8a4113304242a4e7c687e511b372a1ef4699
muazong <giongmua2004@gmail.com>
2024-12-20 16:26:08 +0700

    add image url api

--- src/assets/tokens.js
+++ src/assets/tokens.js
@@ -1,4 +1,5 @@
 const API_KEY = import.meta.env.VITE_API_TOKEN;
 const URL = import.meta.env.VITE_API_MOVIES;
+const IMAGE_URL = import.meta.env.VITE_API_IMAGE;
 
-export { API_KEY, URL };
+export { API_KEY, URL, IMAGE_URL };
