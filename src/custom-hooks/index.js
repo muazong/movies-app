fa1d70314e5f0ce8126153ab2f9a86335927cce0
muazong <giongmua2004@gmail.com>
2024-12-24 09:46:55 +0700

    update custom hooks

--- src/custom-hooks/index.js
+++ src/custom-hooks/index.js
@@ -13,19 +13,21 @@ const options = {
 function useMoviesFetch(type) {
   const [movieData, setMovieData] = useState([]);
 
-  const handleFetch = async () => {
-    try {
-      const response = await fetch(
-        `${URL}movie/${type}?language=en-US&page=1`,
-        options,
-      );
-      const data = await response.json();
-      setMovieData(data.results);
-    } catch (error) {
-      alert("Error fetching movies" + error);
-    }
-  };
-  useEffect(() => handleFetch, []);
+  useEffect(
+    () => async () => {
+      try {
+        const response = await fetch(
+          `${URL}movie/${type}?language=en-US&page=1`,
+          options,
+        );
+        const data = await response.json();
+        setMovieData(data.results);
+      } catch (error) {
+        alert("Error fetching movies" + error);
+      }
+    },
+    [],
+  );
 
   return movieData;
 }
@@ -34,22 +36,24 @@ function useTrailerFetch(id) {
   const [videoData, setVideoData] = useState([]);
   const [detailData, setDetailData] = useState([]);
 
-  const handleFetch = async () => {
-    const video_url = `${URL}movie/${id}/videos?language=en-US`;
-    const detail_url = `${URL}movie/${id}?language=en-US`;
+  useEffect(
+    () => async () => {
+      const video_url = `${URL}movie/${id}/videos?language=en-US`;
+      const detail_url = `${URL}movie/${id}?language=en-US`;
 
-    const [video, detail] = await Promise.all([
-      fetch(video_url, options),
-      fetch(detail_url, options),
-    ]);
+      const [video, detail] = await Promise.all([
+        fetch(video_url, options),
+        fetch(detail_url, options),
+      ]);
 
-    const videoData = await video.json();
-    const detailData = await detail.json();
+      const videoData = await video.json();
+      const detailData = await detail.json();
 
-    setVideoData(videoData.results[0]);
-    setDetailData(detailData);
-  };
-  useEffect(() => handleFetch, []);
+      setVideoData(videoData.results[0]);
+      setDetailData(detailData);
+    },
+    [],
+  );
 
   return { videoData, detailData };
 }
