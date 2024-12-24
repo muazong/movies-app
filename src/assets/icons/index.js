34c5b0bf517165423af4aa43a47419f1262d18d5
muazong <giongmua2004@gmail.com>
2024-12-24 09:48:36 +0700

    update & add more icons

--- src/assets/icons/index.js
+++ src/assets/icons/index.js
@@ -4,6 +4,16 @@ import location_icon from "./location-dot-solid.svg";
 import email_icon from "./envelope-solid.svg";
 import phone_icon from "./phone-solid.svg";
 
+import clock_icon from "./clock-solid.svg";
+import language_icon from "./language-solid.svg";
+import calendar_icon from "./calendar-days-solid.svg";
+import vote_average_icon from "./square-poll-horizontal-solid.svg";
+import vote_count_icon from "./square-poll-vertical-solid.svg";
+
+import revenue_icon from "./chart-simple-solid.svg";
+import budget_icon from "./dollar-sign-solid.svg";
+
+import imdb_icon from "./imdb-brands-solid.svg";
 import facebook_icon from "./facebook-brands-solid.svg";
 import youtube_icon from "./youtube-brands-solid.svg";
 import x_twitter_icon from "./x-twitter-brands-solid.svg";
@@ -19,6 +29,14 @@ const icons = {
   youtube_icon,
   x_twitter_icon,
   snapchat_icon,
+  clock_icon,
+  language_icon,
+  calendar_icon,
+  vote_average_icon,
+  vote_count_icon,
+  imdb_icon,
+  revenue_icon,
+  budget_icon,
 };
 
 export default icons;
