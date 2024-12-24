5ad489b78943f54f0dc3526bfb02b4cb76100099
muazong <giongmua2004@gmail.com>
2024-12-24 09:46:22 +0700

    set scroll to the top

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,8 +1,13 @@
+import { useEffect } from "react";
 import Header from "../../components/Header";
 import Body from "../../components/Body";
 import Footer from "../../components/Footer";
 
 function Home() {
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
+
   return (
     <>
       <Header />
