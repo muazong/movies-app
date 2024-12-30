d1a98672455d2737da428b6a84c61675053c2935
muazong <giongmua2004@gmail.com>
2024-12-30 22:44:09 +0700

    update homebody

---
+++ src/components/Body/HomeBody/Banner.jsx
@@ -0,0 +1,50 @@
+import { useNavigate } from "react-router-dom";
+import { useMovies } from "../../../custom-hooks";
+import PropTypes from "prop-types";
+
+import icons from "../../../assets/icons";
+import { IMAGE_URL } from "../../../assets/tokens";
+
+function Banner({ type = "upcoming" }) {
+  const { movies, isLoading } = useMovies(type);
+  const navigate = useNavigate();
+
+  return movies[0] && !isLoading ? (
+    <div
+      className="banner"
+      style={{
+        backgroundImage: `url(${IMAGE_URL}${movies[0].backdrop_path})`,
+      }}
+    >
+      <div className="background"></div>
+      <div className="info">
+        <div className="text">
+          <h1>{movies[0].original_title}</h1>
+          <p>{movies[0].overview}</p>
+        </div>
+        <button onClick={() => navigate(`/trailer/${movies[0].id}`)}>
+          Watch Now
+        </button>
+      </div>
+      <div
+        className="movie-image"
+        onClick={() => navigate(`/trailer/${movies[0].id}`)}
+      >
+        <img src={icons.play_icon} className="play-icon" alt="" />
+        <img
+          src={IMAGE_URL + movies[0].poster_path}
+          className="poster"
+          alt=""
+        />
+      </div>
+    </div>
+  ) : (
+    ""
+  );
+}
+
+Banner.propTypes = {
+  type: PropTypes.string,
+};
+
+export default Banner;
--- src/components/Body/HomeBody/HomeBody.jsx
+++ src/components/Body/HomeBody/HomeBody.jsx
@@ -1,50 +1,7 @@
-import { useNavigate } from "react-router-dom";
-import { useMoviesFetch } from "../../../custom-hooks";
-
-import icons from "../../../assets/icons";
-import { IMAGE_URL } from "../../../assets/tokens";
-
 import MovieList from "../MovieList";
+import Banner from "./Banner";
 
 import "./HomeBody.scss";
-
-function Banner() {
-  const bannerData = useMoviesFetch("upcoming")[0];
-  const navigate = useNavigate();
-
-  return bannerData ? (
-    <div
-      className="banner"
-      style={{
-        backgroundImage: `url(${IMAGE_URL}${bannerData?.backdrop_path})`,
-      }}
-    >
-      <div className="background"></div>
-      <div className="info">
-        <div className="text">
-          <h1>{bannerData?.original_title}</h1>
-          <p>{bannerData?.overview}</p>
-        </div>
-        <button onClick={() => navigate(`/trailer/${bannerData?.id}`)}>
-          Watch Now
-        </button>
-      </div>
-      <div
-        className="movie-image"
-        onClick={() => navigate(`/trailer/${bannerData?.id}`)}
-      >
-        <img src={icons.play_icon} className="play-icon" alt="" />
-        <img
-          src={IMAGE_URL + bannerData?.poster_path}
-          className="poster"
-          alt=""
-        />
-      </div>
-    </div>
-  ) : (
-    ""
-  );
-}
 
 function HomeBody() {
   return (
