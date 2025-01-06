1766218414793d5a910e9ae0cf2d8045e5484ed5
muazong <giongmua2004@gmail.com>
2025-01-07 00:04:20 +0700

    update

--- src/components/Body/TrailerBody/TrailerVideo/Sales/Sales.jsx
+++ src/components/Body/TrailerBody/TrailerVideo/Sales/Sales.jsx
@@ -1,26 +1,25 @@
-import { useCallback } from "react";
+import { useMemo } from "react";
 import PropTypes from "prop-types";
 
 import icons from "../../../../../assets/icons";
 
 function Sales({ detail }) {
-  const formattedNumber = useCallback((number) => {
-    const result = new Intl.NumberFormat("en-US", {
+  const formatter = useMemo(() => {
+    return new Intl.NumberFormat("en-US", {
       style: "currency",
       currency: "USD",
-    }).format(number);
-    return result;
+    });
   }, []);
 
   return (
     <div className="sales-wrapper">
       <p className="budget">
         <img src={icons.budget_icon} className="icon" alt="" />
-        Budget: {formattedNumber(detail.budget)}
+        Budget: {formatter.format(detail.budget)}
       </p>
       <p className="revenue">
         <img src={icons.revenue_icon} className="icon" alt="" />
-        Revenue: {formattedNumber(detail.revenue)}
+        Revenue: {formatter.format(detail.revenue)}
       </p>
     </div>
   );
