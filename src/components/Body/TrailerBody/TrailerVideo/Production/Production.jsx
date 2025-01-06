0207dbef0f8640936c5b1e315f94108d0f5046c6
muazong <giongmua2004@gmail.com>
2025-01-07 00:05:22 +0700

    update

--- src/components/Body/TrailerBody/TrailerVideo/Production/Production.jsx
+++ src/components/Body/TrailerBody/TrailerVideo/Production/Production.jsx
@@ -5,7 +5,7 @@ function Production({ detail }) {
     <div className="production">
       <div className="companies">
         <p>Companies: </p>
-        {detail.production_companies?.map((company, index, array) => {
+        {detail.production_companies.map((company, index, array) => {
           return (
             <span key={company.id}>
               {company.name + (index === array.length - 1 ? "" : ", ")}
